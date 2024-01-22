2a4b5b5e49e6dc637be462f17933c1d5c84591e0
Duy Lam <duylamjunpio@gmail.com>
2024-01-22 14:12:50 +0700

    QLBH

--- QLBH/routes/web.php
+++ QLBH/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +15,31 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//Auth
+Route::controller(AuthController::class)->group(function () {
+    Route::get('register', 'register')->name('register');
+    Route::post('register', 'registerSave')->name('register.save');
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::get('login', 'login')->name('login');
+    Route::post('login', 'loginAction')->name('login.action');
+
+    Route::get('logout', 'logout')->middleware('auth')->name('logout');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('products.index');
+    })->name('products.index');
+
+    //products
+    Route::get('/', [ProductController::class, 'index'])->name('products.index');
+    Route::match(['GET', 'POST'], '/add', [ProductController::class, 'store'])->name('products.add');
+    Route::match(['GET', 'POST'], '/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
+    Route::get('/product.delete/{id}', [ProductController::class, 'destroy'])->name('products.delete');
+
+    //categories
+    Route::get('/categories', [CategoriesController::class, 'index'])->name('categories.index');
+    Route::match(['GET', 'POST'], '/categories/add', [CategoriesController::class, 'store'])->name('categories.add');
+    Route::match(['GET', 'POST'], '/categories/edit/{id}', [CategoriesController::class, 'edit'])->name('categories.edit');
+    Route::get('/categories.delete/{id}', [CategoriesController::class, 'destroy'])->name('categories.delete');
 });
