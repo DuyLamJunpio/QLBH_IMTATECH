2a56cfdf4f650ca46d8b51a95f72d567e73950fd
Trung Hieu Ngo <117133479+TrungHieu1812@users.noreply.github.com>
2024-01-24 14:32:02 +0700

    giao dien login regitster

--- QLBH/resources/views/Auth/login.blade.php
+++ QLBH/resources/views/Auth/login.blade.php
@@ -8,17 +8,11 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Login</title>
-    <!-- Custom fonts for this template-->
-    <link href="{{ asset('admin_assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="{{ asset('admin_assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
-<body class="bg-gradient-primary">
+<body class="bg-info bg-gradient">
     <div class="container">
         <!-- Outer Row -->
         <div class="row justify-content-center">
@@ -27,8 +21,7 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
+                            <div class="col">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
@@ -44,17 +37,17 @@
                                                 </ul>
                                             </div>
                                         @endif
-                                        <div class="form-group">
+                                        <div class="form-group mb-2">
                                             <input name="email" type="email" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
                                                 placeholder="Enter Email Address...">
                                         </div>
-                                        <div class="form-group">
+                                        <div class="form-group mb-2">
                                             <input name="password" type="password"
                                                 class="form-control form-control-user" id="exampleInputPassword"
                                                 placeholder="Password">
                                         </div>
-                                        <div class="form-group">
+                                        <div class="form-group mb-2">
                                             <div class="custom-control custom-checkbox small">
                                                 <input name="remember" type="checkbox" class="custom-control-input"
                                                     id="customCheck">
@@ -62,7 +55,7 @@
                                                     Me</label>
                                             </div>
                                         </div>
-                                        <button type="submit" class="btn btn-primary btn-block btn-user">Login</button>
+                                        <button type="submit" class="btn btn-info w-100 btn-user">Login</button>
                                     </form>
                                     <hr>
                                     <div class="text-center">
@@ -76,13 +69,11 @@
             </div>
         </div>
     </div>
-    <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('admin_assets/vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('admin_assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <!-- Core plugin JavaScript-->
-    <script src="{{ asset('admin_assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-    <!-- Custom scripts for all pages-->
-    <script src="{{ asset('admin_assets/js/sb-admin-2.min.js') }}"></script>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- QLBH/resources/views/Auth/register.blade.php
+++ QLBH/resources/views/Auth/register.blade.php
@@ -7,32 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>SB Admin 2 - Register</title>
-    <!-- Custom fonts for this template-->
-    <link href="{{ asset('admin_assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="{{ asset('admin_assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
-<body class="bg-gradient-primary">
+<body class="bg-info bg-gradient">
     <div class="container">
         <div class="card o-hidden border-0 shadow-lg my-5">
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
-                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
-                    <div class="col-lg-7">
+                    <div class="col">
                         <div class="p-5">
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                             </div>
                             <form action="{{ route('register.save') }}" method="POST" class="user">
                                 @csrf
-                                <div class="form-group">
+                                <div class="form-group mb-2">
                                     <input name="name" type="text"
                                         class="form-control form-control-user @error('name')is-invalid @enderror"
                                         id="exampleInputName" placeholder="Name">
@@ -40,7 +33,7 @@
                                         <span class="invalid-feedback">{{ $message }}</span>
                                     @enderror
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group mb-2">
                                     <input name="email" type="email"
                                         class="form-control form-control-user @error('email')is-invalid @enderror"
                                         id="exampleInputEmail" placeholder="Email Address">
@@ -48,7 +41,7 @@
                                         <span class="invalid-feedback">{{ $message }}</span>
                                     @enderror
                                 </div>
-                                <div class="form-group row">
+                                <div class="form-group mb-2 row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
                                         <input name="password" type="password"
                                             class="form-control form-control-user @error('password')is-invalid @enderror"
@@ -65,18 +58,8 @@
                                             <span class="invalid-feedback">{{ $message }}</span>
                                         @enderror
                                     </div>
-                                    <div class="form-group" role="group" aria-label="Basic radio toggle button group">
-                                        <input type="radio" class="btn-check" name="role" id="btnradio1"
-                                            autocomplete="off" value="Admin" checked>
-                                        <label class="btn btn-outline-primary" for="btnradio1">Admin</label>
-
-                                        <input type="radio" class="btn-check" name="role" id="btnradio2"
-                                            autocomplete="off" value="Staff">
-                                        <label class="btn btn-outline-primary" for="btnradio2">Staff</label>
-
-                                    </div>
                                 </div>
-                                <button type="submit" class="btn btn-primary btn-user btn-block">Register
+                                <button type="submit" class="btn btn-info w-100  btn-user">Register
                                     Account</button>
                             </form>
                             <hr>
@@ -89,15 +72,10 @@
             </div>
         </div>
     </div>
-    <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('admin_assets/vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('admin_assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-    <!-- Core plugin JavaScript-->
-    <script src="{{ asset('admin_assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="{{ asset('admin_assets/js/sb-admin-2.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
